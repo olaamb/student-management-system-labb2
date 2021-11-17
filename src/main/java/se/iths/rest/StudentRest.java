18e0bbe7fdc122c37fd4a7e2d494e51f09a6345f
Ola Amberntsson <ola.amberntsson@iths.se>
2021-11-17 16:08:05 +0100

    json response works in lastname method. but the method itself is ugly

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -5,7 +5,6 @@ import se.iths.entity.Student;
 import se.iths.service.StudentService;
 
 import javax.inject.Inject;
-import javax.json.stream.JsonParsingException;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,7 +25,7 @@ public class StudentRest {
             studentService.createStudent(student);
             return Response.ok(student).build();
         } catch (WebApplicationException w) {
-            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).build();
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON).build();
         }
     }
 
@@ -36,25 +35,26 @@ public class StudentRest {
         try {
             List<Student> foundStudents = studentService.getAllStudents();
             if (foundStudents == null || foundStudents.isEmpty()) {
-                return Response.status(Response.Status.NO_CONTENT).type(MediaType.APPLICATION_JSON_TYPE).build();
+                return Response.status(Response.Status.NO_CONTENT).type(MediaType.APPLICATION_JSON).build();
             }
             return Response.ok(foundStudents).build();
         } catch (WebApplicationException w) {
-            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).build();
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON).build();
         }
     }
 
     @Path("query")
     @GET
     public Response getStudentByLastName(@QueryParam("lastname") String lastname) {
+        String responseIfEmpty = "{\"Error\": \"No student with that last name was found.\"}";
         try {
             List<Student> foundStudents = studentService.getStudentByLastname(lastname);
             if (foundStudents == null || foundStudents.isEmpty()) {
-                return Response.status(Response.Status.NO_CONTENT).type(MediaType.APPLICATION_JSON_TYPE).build();
+                throw new WebApplicationException(Response.status(Response.Status.NO_CONTENT).entity(responseIfEmpty).type(MediaType.APPLICATION_JSON).build());
             }
             return Response.ok(foundStudents).build();
-        } catch (WebApplicationException w) {
-            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).build();
+        } catch (WebApplicationException w){
+            return Response.status(Response.Status.BAD_REQUEST).entity(responseIfEmpty).type(MediaType.APPLICATION_JSON).build();
         }
     }
 
@@ -65,7 +65,7 @@ public class StudentRest {
             studentService.updateStudent(student);
             return Response.ok().build();
         } catch (WebApplicationException w) {
-            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).build();
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON).build();
         }
     }
 
@@ -76,7 +76,7 @@ public class StudentRest {
             Student updatedStudent = studentService.updateStudentLastName(id, lastname);
             return Response.ok(updatedStudent).build();
         } catch (WebApplicationException w) {
-            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).build();
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON).build();
         }
     }
 
@@ -87,7 +87,7 @@ public class StudentRest {
             studentService.deleteStudent(id);
             return Response.ok().build();
         } catch (WebApplicationException w) {
-            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).build();
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON).build();
         }
     }
 }
